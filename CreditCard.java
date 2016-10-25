ae3875b559ca927badc00e7f3697bd19c919b045
roshnivp <roshniknambiar@gmail.com>
2016-10-24 21:49:30 -0700

    First changes

---
+++ CreditCard.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Roshni Velluva Puthanidam on 21/10/16.
+ */
+public abstract  class CreditCard {
+    public abstract int cardVerify(String f1,String f2,String f3,String f4,String f5,int l1,int l2,int l3,int l4,int l5);
+
+}
